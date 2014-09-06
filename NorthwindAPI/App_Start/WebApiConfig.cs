f2a5402a49bc4d885d763a65d4bc06f2051d9706
respag <respag@hotmail.com>
2014-09-05 20:20:35 -0500

    Primera

---
+++ NorthwindAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using NorthwindAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace NorthwindAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.EnableCors();
+            // Web API configuration and services
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Category>("Categories");
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ NorthwindAPI/Scripts/app.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="datajs-1.1.3.js" />
+OData.read("http://localhost:4664/Categories", function (data) {
+      var html = "";
+      $.each(data.results, function (c) {
+          html += "<div>" + c.CategoryName + "</div>";
+      });
+      $(html).appendTo($("#target-element-id"));
+  }
+);
